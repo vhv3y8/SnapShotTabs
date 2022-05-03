6016628b096a46a13c9480c6f29db4fb78738dd5
vhv3y8 <>
2022-05-04 00:11:20 +0900

    storage 2, little css

--- src/background.js
+++ src/background.js
@@ -1,19 +1,29 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
+
+  let storageInit = {
     data:
       [
         {
-          time: new Date('December 17, 1995 03:24:00').toISOString().replace("T", " ").slice(0, -5),
+          idx: 1,
+          title: createTitle("chrome.runtime - Chrome Developers", 2),
+          time: parseDate(new Date('December 17, 1995 03:24:00')),
           urls: ["https://developer.chrome.com/docs/extensions/reference/storage/", "https://developer.chrome.com/docs/extensions/mv3/service_workers/"]
         },
         {
-          time: new Date().toISOString().replace("T", " ").slice(0, -5),
+          idx: 2,
+          title: createTitle("Number.prototype.toString() - JavaScript | MDN", 2),
+          time: parseDate(new Date()),
           urls: ["https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice", "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date"]
         }
-      ]
-  }, function () {
-    console.log("onInstalled event - data is set.");
-  });
+      ],
+    lastIdx: 2,
+    window: {}
+  };
+  chrome.storage.sync.set(storageInit)
+    .then(() => {
+      console.log("storage set.");
+      console.log(storageInit);
+    });
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
@@ -25,4 +35,14 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
   }
 });
 
-var hihi = "asdfadf";
+export function parseDate(date) {
+  return date.toISOString().replace("T", " ").slice(0, -5);
+}
+
+export function createTitle(title, arrLength) {
+  if (arrLength == 1) {
+    return `${title.slice(0, 35)}`;
+  } else {
+    return `${title.slice(0, 20)}... and ${arrLength - 1} more`;
+  }
+}
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,17 +1,45 @@
+import { parseDate, createTitle } from "../background.js";
+
 window.addEventListener("load", (e) => {
   chrome.storage.sync.get(["data"], function (result) {
     console.log(result.data);
     for (let idx = 0; idx < result.data.length; idx++) {
-      document.getElementById("list").appendChild(createItemElement(createName(result.data[idx].urls), result.data[idx].time, idx));
+      document.getElementById("list").appendChild(createItemElement(result.data[idx].title, result.data[idx].time, idx));
     }
   });
 });
 
-document.getElementById("buttons").addEventListener("click", (e) => {
-  chrome.storage.sync.get(["data"], function (result) {
-    console.log(result);
-    console.log(result.data);
-  });
+document.getElementById("buttons").addEventListener("click", async (e) => {
+  let winData = await chrome.storage.sync.get(["window"]);
+  winData = winData.window;
+  let storageData = await chrome.storage.sync.get(["data"]);
+  storageData = storageData.data;
+  let curWin = await chrome.windows.getCurrent();
+  let tabs = await chrome.tabs.query({ currentWindow: true });
+  console.log(winData);
+  console.log(curWin);
+  console.log(tabs);
+  let time = new Date();
+  time.setHours(time.getHours() + 9);
+  let newData = { title: createTitle(tabs[0].title, tabs.length), time: parseDate(time), urls: tabs.map(tab => tab.url) };
+
+  if (!Object.keys(winData).includes(curWin.id)) {
+    storageData.push(newData);
+    document.getElementById("list").appendChild(createItemElement(createTitle(tabs[0].title, tabs.length), newData.time, storageData.length));
+  } else {
+
+  }
+  winData[curWin.id.toString()] = newData.urls;
+  chrome.storage.sync.set({ window: winData })
+    .then(() => {
+      console.log("storage 'window' is now set to :");
+      console.log({ window: winData });
+    });
+  chrome.storage.sync.set({ data: storageData })
+    .then(() => {
+      console.log("storage 'data' is now set to :");
+      console.log({ data: storageData });
+    });
 });
 
 function createItemElement(nameString, timeString, idx = -1) {
@@ -26,13 +54,9 @@ function createItemElement(nameString, timeString, idx = -1) {
   timeTag.innerHTML = `updated: ${timeString}`;
   elem.appendChild(nameTag);
   elem.appendChild(timeTag);
+  elem.addEventListener("click", (e) => {
+
+  });
   return elem;
 }
 
-function createName(urlArray) {
-  if (urlArray.length == 1) {
-    return `${urlArray[0].slice(0, 35)}`;
-  } else {
-    return `${urlArray[0].slice(0, 20)}... and ${urlArray.length - 1} more`;
-  }
-}
